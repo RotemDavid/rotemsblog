cd4c5ce7e25593686d83d2f3f71f13aca0264556
Rotem David <rotemm135@gmail.com>
2013-06-04 23:14:12 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rotemsblog::Application.config.secret_token = 'a4ebc4e034b43b2d64945927d6248d6ca58314795388961c72b3a8a984d07a10b937d6677b258cdba2bb47e291dcbcce968180ec6b463d1c396f1032398c2553'
